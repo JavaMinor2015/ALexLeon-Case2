a4398dac17f950470fed10a3b088afcaac8e433c
peaseloxes <peaseloxes@gmail.com>
2015-11-19 08:05:58 -0600

    temp

---
+++ FEGarageManagementSysteem/src/main/java/javaminor/al/beans/CustomerBean.java
@@ -0,0 +1,13 @@
+package javaminor.al.beans;
+
+import java.io.Serializable;
+import javax.inject.Named;
+
+/**
+ * Created by alex on 11/19/15.
+ */
+@Named("customer")
+public class CustomerBean implements Serializable {
+    private static final long serialVersionUID = 8020406095868256398L;
+
+}
