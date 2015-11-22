788b742b75d01c1d3412c72b06e1901490ba0f54
Alex <peaseloxes@gmail.com>
2015-11-22 09:48:38 -0600

    Adding new cars to newly created customers.
    Fixed repository issue where it would try to create already existing items.

--- FEGarageManagementSysteem/src/main/java/javaminor/al/beans/CustomerManagedBean.java
+++ FEGarageManagementSysteem/src/main/java/javaminor/al/beans/CustomerManagedBean.java
@@ -2,10 +2,11 @@ package javaminor.al.beans;
 
 import java.io.Serializable;
 import javaminor.al.domain.beans.CustomerBean;
+import javaminor.al.entities.concrete.Car;
 import javaminor.al.entities.concrete.Driver;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
@@ -19,7 +20,7 @@ import org.apache.log4j.Logger;
  * Created by alex on 11/19/15.
  */
 @Named("customer")
-@RequestScoped
+@SessionScoped
 @Getter
 @Setter
 public class CustomerManagedBean implements Serializable {
@@ -28,6 +29,8 @@ public class CustomerManagedBean implements Serializable {
 
     //TODO refactor to proper Customer with Driver/LeaseCompany option
     private Driver driver;
+
+    private Car car;
 
     @EJB
     private CustomerBean bean;
@@ -38,12 +41,15 @@ public class CustomerManagedBean implements Serializable {
     @PostConstruct
     public void init() {
         driver = new Driver();
+        car = new Car();
     }
 
     /**
-     * Persist the customer.
+     * Persist the customer and go to the next step.
+     *
+     * @return the next page in the process.
      */
-    public void createCustomer() {
+    public String createCustomer() {
         try {
             bean.createCustomer(driver);
         } catch (ConstraintViolationException e) {
@@ -51,9 +57,27 @@ public class CustomerManagedBean implements Serializable {
             FacesContext.getCurrentInstance().addMessage(null, new
                     FacesMessage(e.getConstraintViolations().toString()));
             LOGGER.warn(e.getMessage(), e);
+            return "addCustomer";
         }
 
         FacesContext.getCurrentInstance().addMessage("addCustomer:customerCreateBtn", new
                 FacesMessage("Added customer: " + driver.getFirstName()));
+        return "addCar";
+    }
+
+    /**
+     * Add a car to the current customer and go to the next step.
+     *
+     * @return the next page in the process.
+     */
+    public String addCar() {
+        if(driver.getFirstName() == null) {
+            FacesContext.getCurrentInstance().addMessage("addCustomer:customerCreateBtn", new
+                    FacesMessage("Create a customer first"));
+            return "addCustomer";
+        }
+        FacesContext.getCurrentInstance().addMessage("addCar:carCreateBtn", new
+                FacesMessage("Added car: " + car.getModel()));
+        return "index";
     }
 }
